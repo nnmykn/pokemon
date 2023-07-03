90e15ea5d4e9c0ee6ae07c87ddd2ba1fce83a7d7
ninomiya <hi@kan.run>
2023-07-03 11:49:04 +0900

    ChakraUI追加

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <>
+      <header>
+        <h1>Pokemon図鑑</h1>
+      </header>
+    </>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import Header from "@/components/Header";
+
+// ポケモンのブランドカラーを定義
+const colors = {
+  pokemonYellow: "#FFCB05",
+  pokemonRed: "#FF0000",
+  pokemonBlue: "#0055FF",
+  pokemonGreen: "#00AA00",
+  pokemonBlack: "#000000",
+  pokemonWhite: "#FFFFFF",
+};
+
+const theme = extendTheme({ colors });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    // ChakraProviderでChakra UIのコンポーネントを利用可能にする
+    <ChakraProvider theme={theme}>
+      <Header />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
